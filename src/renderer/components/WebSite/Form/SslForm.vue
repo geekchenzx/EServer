e2b0d0b9a133001afa5497a9d53fa107fc0fc0a1
陈志祥 <chenzx023@qq.com>
2026-08-14 16:22:45 +0800

    feat: 抽取网站SSL表单组件

---
+++ src/renderer/components/WebSite/Form/SslForm.vue
@@ -0,0 +1,73 @@
+<template>
+  <a-form
+    ref="formRef"
+    :model="formData"
+    name="ssl"
+    autocomplete="off"
+    :label-col="{ span: labelColSpan }"
+    :wrapper-col="{ span: wrapperColSpan }"
+  >
+    <a-form-item :label="t('Port')" name="port" :rules="[{ required: true, type: 'number', min: 1, max: 65535 }]">
+      <a-input-number v-model:value="formData.port" min="1" max="65535" />
+    </a-form-item>
+
+    <a-form-item :label="mt('Certificate')" name="certPath" :rules="certRules">
+      <input-open-file-dialog v-model:value="formData.certPath" :toForwardSlash="true" />
+    </a-form-item>
+
+    <a-form-item label="Key" name="keyPath" :rules="keyRules">
+      <input-open-file-dialog v-model:value="formData.keyPath" :toForwardSlash="true" />
+    </a-form-item>
+
+    <a-form-item :label="mt('Force', 'ws') + 'Https'" name="isForceHttps">
+      <a-switch v-model:checked="formData.isForceHttps" />
+    </a-form-item>
+  </a-form>
+</template>
+
+<script setup>
+import { ref, computed } from 'vue'
+import InputOpenFileDialog from '@/renderer/components/Input/InputOpenFileDialog.vue'
+import { mt, t } from '@/renderer/utils/i18n'
+import { useMainStore } from '@/renderer/store'
+
+const props = defineProps({
+  modelValue: { type: Object, required: true },
+  optional: { type: Boolean, default: false }
+})
+const emit = defineEmits(['update:modelValue'])
+
+const store = useMainStore()
+const formRef = ref()
+const labelColSpan = store.settings.Language === 'zh' ? 6 : 10
+const wrapperColSpan = store.settings.Language === 'zh' ? 18 : 14
+
+const formData = computed({
+  get: () => props.modelValue,
+  set: (val) => emit('update:modelValue', val)
+})
+
+const certRules = [
+  {
+    validator: async (_rule, value) => {
+      if (props.optional && !value && !formData.value.keyPath) return Promise.resolve()
+      if (!value) return Promise.reject(t('cannotBeEmpty'))
+      return Promise.resolve()
+    }
+  }
+]
+
+const keyRules = [
+  {
+    validator: async (_rule, value) => {
+      if (props.optional && !formData.value.certPath && !value) return Promise.resolve()
+      if (!value) return Promise.reject(t('cannotBeEmpty'))
+      return Promise.resolve()
+    }
+  }
+]
+
+defineExpose({
+  validate: () => formRef.value.validate()
+})
+</script>
